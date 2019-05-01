50dc3d270614f9030a3457a75218980f893a2461
Admin2 <Admin2@DESKTOP-CO7K5PJ>
2019-05-01 22:46:11 +0300

    #TDP-9 Creating 3 testing file
    Creating 3 file with all tests for this module

--- AppTest/Testing2.cs
+++ AppTest/Testing2.cs
@@ -1,9 +1,80 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Xunit;
+using NLayerApp.BLL.DTO;
+using NLayerApp.WEB.Controllers;
+using NLayerApp.BLL.Interfaces;
+using NLayerApp.WEB.Models;
+using Moq;
+using Microsoft.Owin.Security;
+using NLayerApp.BLL.Services;
+using System.Web.Http.Results;
+using NLayerApp.DAL.Entities;
 
 namespace AppTest
 {
-    class Testing2
+    public class Testing2
     {
+        [Fact]
+        public void GetReportReturnsBadRequestResultWhenIdIsNull()
+        {
+            // Arrange
+            var mock = new Mock<IReportService>();
+            var controller = new HomeController(mock.Object);
+
+            // Act
+            var result = controller.GetReport(null) as HttpStatusCodeResult;
+
+            // Arrange
+            Assert.IsType<HttpStatusCodeResult>(result);
+            Assert.Equal(400, result.StatusCode);
+            Assert.Equal("BadRequest", result.StatusDescription);
+        }
+
+        [Fact]
+        public void GetReportReturnsNotFoundResultWhenReportNotFound()
+        {
+            int ReportId = 1;
+            var mock = new Mock<IReportService>();
+            mock.Setup(repo => repo.GetReport(ReportId))
+                .Returns(null as ReportDTO);
+            var controller = new HomeController(mock.Object);
+
+            var result = controller.GetReport(ReportId) as HttpStatusCodeResult;
+
+            Assert.IsType<HttpStatusCodeResult>(result);
+            Assert.Equal(404, result.StatusCode);
+            Assert.Equal("NotFound", result.StatusDescription);
+        }
+
+        [Fact]
+        public void GetReportReturnsViewResultWithReport()
+        {
+            int ReportId = 2;
+            var mock = new Mock<IReportService>();
+            mock.Setup(repo => repo.GetReport(ReportId))
+                .Returns(GetTestReports().FirstOrDefault(p => p.Id == ReportId));
+            var controller = new HomeController(mock.Object);
+
+            var result = controller.GetReport(ReportId);
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<ReportDTO>(viewResult.ViewData.Model);
+            Assert.Equal("20.02.2019", model.Date);
+            Assert.Equal("Kherson", model.City);
+            Assert.Equal("OlegButin@gmail.com", model.Worker);
+            Assert.Equal(200, model.O3);
+            Assert.Equal(100, model.NO2);
+            Assert.Equal(89, model.SO2);
+            Assert.Equal(ReportId, model.Id);
+
+            
+        }
+
         [Fact]
         public void IndexReturnsAViewResultWithAListOfReports()
         {
@@ -71,5 +142,6 @@ namespace AppTest
         };
             return reports;
         }
+
     }
 }
---
+++ AppTest/Testing3.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using NLayerApp.BLL.DTO;
+using NLayerApp.WEB.Controllers;
+using NLayerApp.BLL.Interfaces;
+using NLayerApp.WEB.Models;
+using Moq;
+using Microsoft.Owin.Security;
+using NLayerApp.BLL.Services;
+using System.Web.Http.Results;
+using NUnit.Framework;
+using System.Web;
+using System.Collections.Specialized;
+using System.Web.Routing;
+
+namespace AppTest.Controllers
+{
+    public static class Test3
+    {
+
+        public static Mock<HttpContextBase> MockHttpContext(string path)
+        {
+            var mockHttpCtx = new Mock<HttpContextBase>();
+            var mockReq = new Mock<HttpRequestBase>();
+            mockReq.Setup(x => x.AppRelativeCurrentExecutionFilePath)
+                   .Returns(path);
+            mockReq.Setup(x => x.Headers).Returns(new NameValueCollection());
+            mockReq.Setup(x => x.Cookies).Returns(new HttpCookieCollection());
+            mockReq.Setup(x => x.QueryString).Returns(new NameValueCollection());
+            mockReq.Setup(x => x.Form).Returns(new NameValueCollection());
+            mockHttpCtx.Setup(x => x.Request).Returns(mockReq.Object);
+
+            var mockResp = new Mock<HttpResponseBase>();
+            mockResp.Setup(x => x.ApplyAppPathModifier(It.IsAny<string>()))
+                    .Returns<string>(x => x);
+            mockResp.Setup(x => x.Cookies).Returns(new HttpCookieCollection());
+            mockHttpCtx.Setup(x => x.Response).Returns(mockResp.Object);
+
+            return mockHttpCtx;
+        }
+
+        public static Mock<HttpContextBase> MockControllerContext(
+                              this Controller controller, string path = null)
+        {
+            var mockHttpCtx = Test3.MockHttpContext(path);
+            var requestCtx = new RequestContext(mockHttpCtx.Object, new RouteData());
+            var controllerCtx = new ControllerContext(requestCtx, controller);
+            controller.ControllerContext = controllerCtx;
+            return mockHttpCtx;
+        }
+
+        [Test]
+        public static void  MakeChangesPost_ReturnsARedirect_WhenModelStateIsValid()
+        {
+            // Arrange
+            var mockRepo = new Mock<IReportService>();
+            var controller = new HomeController(mockRepo.Object);
+            ReportViewModel rep = new ReportViewModel();
+            var ctx = controller.MockControllerContext();
+            ctx.Object.Request.QueryString.Set("email", "semionviktoria@gmail.com");
+            // ContextMocks.Request.SetupGet(r => r["mymodel"]).Returns(email);
+
+            // Act
+            System.Web.Mvc.RedirectToRouteResult redirectResult =
+     (System.Web.Mvc.RedirectToRouteResult)controller.MakeChanges(rep);
+
+            // Assert
+            Assert.IsInstanceOf<System.Web.Mvc.RedirectToRouteResult>(redirectResult);
+            Assert.AreEqual("Home/Index", redirectResult.RouteValues["controller"]);
+        }
+
+        [Test]
+        public static void SetReportPost_ReturnsARedirect_WhenModelStateIsValid()
+        {
+            // Arrange
+            var mockRepo = new Mock<IReportService>();
+            var controller = new HomeController(mockRepo.Object);
+            ReportViewModel rep = new ReportViewModel();
+            var ctx = controller.MockControllerContext();
+            ctx.Object.Request.QueryString.Set("email", "semionviktoria@gmail.com");
+            // ContextMocks.Request.SetupGet(r => r["mymodel"]).Returns(email);
+
+            // Act
+            System.Web.Mvc.RedirectToRouteResult redirectResult =
+     (System.Web.Mvc.RedirectToRouteResult)controller.SetReport(rep);
+
+            // Assert
+            Assert.IsInstanceOf<System.Web.Mvc.RedirectToRouteResult>(redirectResult);
+            Assert.AreEqual("Home/Index", redirectResult.RouteValues["controller"]);
+        }
+
+        [Test]
+        public static void DeleteReportPost_ReturnsARedirect_WhenModelStateIsValid()
+        {
+            // Arrange
+            var mockRepo = new Mock<IReportService>();
+            var controller = new HomeController(mockRepo.Object);
+            ReportViewModel rep = new ReportViewModel();
+            var ctx = controller.MockControllerContext();
+            ctx.Object.Request.QueryString.Set("email", "semionviktoria@gmail.com");
+            // ContextMocks.Request.SetupGet(r => r["mymodel"]).Returns(email);
+
+            // Act
+            System.Web.Mvc.RedirectToRouteResult redirectResult =
+     (System.Web.Mvc.RedirectToRouteResult)controller.DeleteReport(rep);
+
+            // Assert
+            Assert.IsInstanceOf<System.Web.Mvc.RedirectToRouteResult>(redirectResult);
+            Assert.AreEqual("Home/Index", redirectResult.RouteValues["controller"]);
+        }
+    }
+
+
+}
